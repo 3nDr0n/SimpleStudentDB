52e60fc7d2f1313dfc7295f5a34f5e4261fa2247
Alexander Magyar <magyarali@gmail.com>
2024-02-04 16:36:37 +0000

    Add project files.

---
+++ FinalAssignment/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Diagnostics;
+
+namespace StudentAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var schoolDb = new SchoolContext())
+            {
+                Console.Write("Please enter the new student's name: ");
+                var name = Console.ReadLine();
+                Console.WriteLine("Now, Please enter their date of birth following the format of YYYY/MM/DD:");
+                var DoB = Console.ReadLine();
+                var parsedDate = DateTime.Parse(DoB);
+                Console.WriteLine("Please enter what grade they are currently in:");
+                var grade = Console.ReadLine();
+                var newStudent = new Student() { StudentName = name, DateOfBirth = parsedDate, Grade = grade };
+                schoolDb.Students.Add(newStudent);
+                schoolDb.SaveChanges();
+            }
+        }
+    }
+
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public string Grade { get; set; }
+    }
+
+
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base()
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
